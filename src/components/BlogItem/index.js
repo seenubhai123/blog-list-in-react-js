ef1ca4871b0a49307e52251afdeffb5babc46f73
seenubhai123 <thattesreenivasulu7876@gmail.com>
2023-04-11 00:12:18 +0000

    Blog List in react js

---
+++ src/components/BlogItem/index.js
@@ -0,0 +1,19 @@
+import './index.css'
+
+const BlogItem = props => {
+  const {each} = props
+  const {title, description, publishedDate} = each
+
+  return (
+    <li className="list-item">
+      <div className="heading-date-container">
+        <h1 className="profile-heading">{title}</h1>
+        <p className="profile-para">{publishedDate}</p>
+      </div>
+      <p className="profile-para">{description}</p>
+      <hr className="line" />
+    </li>
+  )
+}
+
+export default BlogItem
